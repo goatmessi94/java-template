89a446c2d8a7e35b45ef05b229d93cee906f6598
asj2 <asj@bizspring.co.kr>
2024-02-22 00:15:33 +0900

    first commit

---
+++ src/test/java/kr/co/example/javatemplate/JavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.example.javatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
